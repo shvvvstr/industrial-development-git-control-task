277003ddbe6a6552288b37b5377c17f0a757ab67
Уханова Яна Владимировна <y.uhanova@univ.priv>
2021-09-16 15:21:36 +0300

    new-config to new-feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!1");
+        System.out.println("Hello, world!\n");
         System.out.println("New Feature!");
     }
 }
